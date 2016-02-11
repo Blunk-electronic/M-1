ff2edadcac4a3b60b17879fb67a4cd37a775d022
Mario Blunk <mario.blunk@blunk-electronic.de>
2016-02-11 11:34:15 +0100

    added license header

--- src/udbinfo/udbinfo.adb
+++ src/udbinfo/udbinfo.adb
@@ -1,3 +1,28 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                    SYSTEM M-1 MODULE UDBINFO                             --
+--                                                                          --
+--                                 M-1                                      --
+--                                                                          --
+--                               B o d y                                    --
+--                                                                          --
+--         Copyright (C) 2016 Mario Blunk, Blunk electronic                 --
+--                                                                          --
+--    This program is free software: you can redistribute it and/or modify  --
+--    it under the terms of the GNU General Public License as published by  --
+--    the Free Software Foundation, either version 3 of the License, or     --
+--    (at your option) any later version.                                   --
+--                                                                          --
+--    This program is distributed in the hope that it will be useful,       --
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of        --
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         --
+--    GNU General Public License for more details.                          --
+--                                                                          --
+--    You should have received a copy of the GNU General Public License     --
+--    along with this program.  If not, see <http://www.gnu.org/licenses/>. --
+------------------------------------------------------------------------------
+
+
 with Ada.Text_IO;			use Ada.Text_IO;
 with Ada.Integer_Text_IO;	use Ada.Integer_Text_IO;
 with Ada.Float_Text_IO;		use Ada.Float_Text_IO;
