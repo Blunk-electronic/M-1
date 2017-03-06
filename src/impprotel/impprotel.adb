7ee9e51d505584f4d944b03714f25408e3597616
Mario Blunk <mario.blunk@blunk-electronic.de>
2017-03-06 19:29:36 +0100

    assembly variants

--- src/impprotel/impprotel.adb
+++ src/impprotel/impprotel.adb
@@ -36,13 +36,14 @@ with ada.text_io;				use ada.text_io;
 -- with Ada.Characters.Handling; 	use Ada.Characters.Handling;
 
 with ada.strings.bounded; 		use ada.strings.bounded;
+with ada.strings.unbounded;		use ada.strings.unbounded;
 --with Ada.Strings.fixed; 		use Ada.Strings.fixed;
 with ada.containers;            use ada.containers;
 with ada.containers.vectors;
 with ada.exceptions; 			use ada.exceptions;
 --  
 with ada.command_line;			use ada.command_line;
--- with ada.directories;			use ada.directories;
+with ada.directories;			use ada.directories;
 
 --with m1;
 with m1_internal;               use m1_internal;
@@ -76,7 +77,12 @@ procedure impprotel is
     type type_device is record
         name    : type_device_name.bounded_string;
         packge  : type_package_name.bounded_string;
-        value   : type_value.bounded_string;
+		value   : type_value.bounded_string;
+		--		number_of_variants	: positive := 1;
+		has_variants	: boolean := false;
+		variant_id		: positive := 1;
+		mounted			: boolean := false;
+		processed		: boolean := false;
     end record;
 
     package type_list_of_devices is new vectors ( index_type => positive, element_type => type_device);
@@ -111,18 +117,254 @@ procedure impprotel is
     type type_net_item is (name, pin);
     net_item_next : type_net_item;
 
+	-- ASSEMBLY VARIANTS
+	type type_assembly_variant is record
+		name		: type_device_name.bounded_string;
+		position	: positive;
+		packge		: type_package_name.bounded_string;
+		value		: type_value.bounded_string; -- CS: package and value should be read and verified
+		active		: boolean := false;
+		processed	: boolean := false;
+	end record;
 
-    procedure test_assembly_variants is
-    begin
-        if length(list_of_devices) > 0 then
-            for d in 1..length(list_of_devices) loop
-                null;
+	package type_list_of_assembly_variants is new vectors (positive, type_assembly_variant);
+	use type_list_of_assembly_variants;
+	list_of_assembly_variants : type_list_of_assembly_variants.vector;
+		
+
+	procedure manage_assembly_variants is
+		l : natural := natural(length(list_of_devices));
+		np : type_device_name.bounded_string;
+		variants_found : boolean := false;
+		file_variants : ada.text_io.file_type;
+		file_list_of_assembly_variants : unbounded_string;
+		package type_pin_name is new generic_bounded_length(length_of_pin_name);
+		use type_pin_name;
+		package type_list_of_pins is new vectors ( index_type => positive, element_type => type_pin_name.bounded_string);
+		
+-- 		procedure increment_variants (device : in out type_device) is
+-- 		begin
+-- 			device.number_of_variants := device.number_of_variants + 1;
+-- 		end increment_variants;
+
+		procedure mark_as_having_variants (device : in out type_device) is
+		begin
+			device.has_variants := true;
+		end mark_as_having_variants;
+		
+		procedure mark_as_processed (device : in out type_device) is
+		begin
+			device.processed := true;
+		end mark_as_processed;
+
+		device_scratch : type_device;
+
+		procedure read_assembly_variants is
+			--package type_line_of_assembly_variants is new generic_bounded_length(300); use type_line_of_assembly_variants;
+			line_counter : natural := 0;
+			line : extended_string.bounded_string;
+			assembly_variant_scratch : type_assembly_variant;
+		begin
+			open (file_variants, in_file, to_string(file_list_of_assembly_variants));
+			set_input(file_variants);
+			while not end_of_file loop
+				line := extended_string.to_bounded_string(get_line);
+				line := remove_comment_from_line(line);
+				if get_field_count(extended_string.to_string(line)) > 0 then -- skip empty lines
+
+					-- read assembly variant from a line like "R3 RESC1005X40N 12K active"
+					assembly_variant_scratch.name := to_bounded_string(get_field(extended_string.to_string(line),1));
+					assembly_variant_scratch.packge := to_bounded_string(get_field(extended_string.to_string(line),2));
+					assembly_variant_scratch.value := to_bounded_string(get_field(extended_string.to_string(line),3));
+					if get_field(extended_string.to_string(line),4) = "active" then -- CS: output error when typing error ?
+						assembly_variant_scratch.active := true;
+					else
+						assembly_variant_scratch.active := false;
+					end if;
+
+					-- append assembly variant to list
+					append(list_of_assembly_variants,assembly_variant_scratch);
+
+					-- purge temporarly assembly variant
+					assembly_variant_scratch.name := to_bounded_string("");
+					assembly_variant_scratch.packge := to_bounded_string("");					
+					assembly_variant_scratch.value := to_bounded_string("");					
+				end if;
+			end loop;
+			set_input(standard_input);
+			close(file_variants);
+		end read_assembly_variants;
+
+		procedure set_position_of_active_variants is
+			l : natural := natural(length(list_of_assembly_variants));
+			vp, vs : type_assembly_variant;
+			p : positive := 1; -- position of variant
+
+			procedure mark_variant_as_processed (variant : in out type_assembly_variant) is
+			begin variant.processed := true; end mark_variant_as_processed;
+
+			procedure set_position (variant : in out type_assembly_variant) is
+			begin variant.position := p; end set_position;
+			
+		begin
+			for ap in 1..l loop
+				vp := element(list_of_assembly_variants,ap); -- first occurence
+				if not vp.processed then -- skip already processed variants
+					if vp.active then -- if first variant active, set position as proposed by p
+						update_element(list_of_assembly_variants,ap,set_position'access);
+						for as in ap+1..l loop -- serach for further occurences and mark them as processed
+							vs := element(list_of_assembly_variants,as);
+							if vp.name = vs.name then -- on name match
+								-- mark this variant as processed
+								update_element(list_of_assembly_variants,as,mark_variant_as_processed'access);
+							end if;
+						end loop;
+					else -- first variant not active
+						for as in ap+1..l loop -- serach for further occurences
+							vs := element(list_of_assembly_variants,as);
+							if vp.name = vs.name then -- on name match
+								p := p + 1; -- increment position on match
+								if vs.active then -- if variant active, set position as proposed by p
+									update_element(list_of_assembly_variants,as,set_position'access);
+									p := 1; -- reset position
+								end if;
+								-- mark this variant and all others as processed
+								update_element(list_of_assembly_variants,as,mark_variant_as_processed'access);
+							end if;
+						end loop;
+					end if;
+				end if;
+			end loop;
+		end set_position_of_active_variants;
+
+		procedure apply_assembly_variants is
+			la : positive := natural(length(list_of_assembly_variants));
+			position_in_device_list : natural := 0;
+			position_in_variant_list : natural := 0;
+			active_variant_found : boolean := false;
+			v : type_assembly_variant;
+
+			procedure mark_device_as_mounted (device : in out type_device) is
+			begin device.mounted := true; end mark_device_as_mounted;
+
+			procedure set_variant_id (device : in out type_device) is
+			begin device.variant_id := position_in_variant_list; end set_variant_id;
+			
+		begin
+			for d in 1..l loop -- loop in device list
+				device_scratch := element(list_of_devices,d); -- save element temporarly in device_scratch
+				if device_scratch.has_variants then -- if device has variants, search in variants list for that device
+					position_in_device_list := position_in_device_list + 1; -- update position of occurences in device list
+
+					-- search current device in variant list
+					for a in 1..la loop
+						v := element(list_of_assembly_variants,a); -- save current variant temporarly in v
+						if to_string(device_scratch.name) = to_string(v.name) then 
+							-- on match update position of occurence in varant list
+							position_in_variant_list := position_in_variant_list + 1;
+							if v.active then -- active variant found, abort search 
+								active_variant_found := true;
+								exit;
+							end if;
+						end if;
+					end loop;
+					if not active_variant_found then
+						put_line(standard_output,message_error & "No active variant found for device '" & 
+							type_device_name.to_string(device_scratch.name));
+						raise constraint_error;
+					end if;
+
+					if position_in_variant_list = position_in_device_list then
+						null; -- active device found
+						update_element(list_of_devices,d,mark_device_as_mounted'access);
+						update_element(list_of_devices,d,set_variant_id'access);
+					end if;
+				else
+					null; -- no variants
+				end if;
+
+				position_in_device_list := 1;
+			end loop;
+		end apply_assembly_variants;
+		
+	begin
+		-- count occurences of devices
+        if l > 0 then
+			for dp in 1..l loop
+				if not element(list_of_devices,dp).processed then -- skip already processed devices
+					np := element(list_of_devices,dp).name;
+					for ds in dp+1..l loop
+						if element(list_of_devices,ds).name = np then
+							variants_found := true;
+							update_element(list_of_devices,dp,mark_as_having_variants'access);							
+							update_element(list_of_devices,ds,mark_as_having_variants'access);
+							update_element(list_of_devices,ds,mark_as_processed'access);
+						end if;
+					end loop;
+				end if;
             end loop;
         else
-            put_line(message_warning & "no devices found !");
-        end if;
-    end test_assembly_variants;
-    
+			write_message(
+				file_handle => file_skeleton, text => message_warning & " no devices found !", console => true, identation => 1);
+		end if;
+
+		if variants_found then
+			write_message(
+				 file_handle => file_skeleton, text => message_warning & "Design has assembly variants:", console => true, identation => 1);
+
+			file_list_of_assembly_variants := to_unbounded_string( compose(
+							containing_directory => name_directory_cad, 
+							name => simple_name(universal_string_type.to_string(name_file_cad_net_list)),
+							extension => file_extension_assembly_variants));
+			
+			if not exists(to_string(file_list_of_assembly_variants)) then
+				put_line(standard_output,"Creating list of assembly variants in " & to_string(file_list_of_assembly_variants));
+				create (file_variants, out_file, to_string(file_list_of_assembly_variants));
+				
+				put_line(file_variants," -- assembly variants of netlist '" & simple_name(universal_string_type.to_string(name_file_cad_net_list)) & "'");
+				put_line(file_variants," -- created by impprotel version " & version);
+				put_line(file_variants," -- date " & date_now);
+				put_line(file_variants, row_separator_0);
+				put_line(file_variants," -- name , package , value");
+				
+				for d in 1..l loop
+					device_scratch := element(list_of_devices,d);
+					if device_scratch.has_variants then
+						write_message(file_handle => file_skeleton, 
+							text => "device: " & to_string(device_scratch.name) &
+									" package: " & to_string(device_scratch.packge) &
+									" value: " & to_string(device_scratch.value),
+							console => true, identation => 2);
+
+						write_message(file_handle => file_variants, 
+							text => to_string(device_scratch.name) & " " &
+									to_string(device_scratch.packge) & " " &
+									to_string(device_scratch.value),
+							console => false, identation => 2);
+					end if;
+				end loop;
+				put_line(file_variants," -- end of variants");
+				close(file_variants);
+			else
+				put_line(standard_output,"Applying active assembly variants from " & to_string(file_list_of_assembly_variants));
+				write_message(
+					file_handle => file_skeleton, text => "as specified in file " & to_string(file_list_of_assembly_variants), identation => 1);
+				read_assembly_variants;
+
+				--apply_assembly_variants;
+			end if;
+		
+			
+		end if;
+    end manage_assembly_variants;
+
+
+	procedure write_skeleton is
+	begin
+		set_output(file_skeleton);
+		
+	end write_skeleton;
+	
 -------- MAIN PROGRAM ------------------------------------------------------------------------------------
 
 begin
@@ -142,6 +384,15 @@ begin
 
 	prog_position	:= 40;
 
+	create (file => file_skeleton, mode => out_file, name => name_file_skeleton);
+	set_output(file_skeleton);
+	put_line(section_mark.section & " info");
+	put_line(" -- netlist skeleton");
+	put_line(" -- created by impprotel version " & version);
+	put_line(" -- date " & date_now);
+	put_line(row_separator_0);
+	set_output(standard_output);
+
     open (file => file_cad_net_list, mode => in_file, name => universal_string_type.to_string(name_file_cad_net_list));
     set_input(file_cad_net_list);
     while not end_of_file loop
@@ -151,25 +402,28 @@ begin
             --put_line("line:>" & to_string(line) & "<");
             
             -- READ DEVICES (NAME, PACKAGE, VALUE)
-            if not device_entered then
-                --put_line(to_string(line));
+			if not device_entered then
+				prog_position	:= 70;
+                --put_line(to_string(line)); -- dbg
                 if get_field(text_in => to_string(line), position => 1) = "[" then
                     --put_line("entering device...");
                     device_entered := true;
                     device_attribute_next := name;
                 end if;
             else -- we are inside a device section
-                put_line(to_string(line));
-                if get_field(text_in => to_string(line), position => 1) = "]" then
+                --put_line(to_string(line));
+				if get_field(text_in => to_string(line), position => 1) = "]" then
+					prog_position	:= 50;
                     device_entered := false; -- we are leaving a device section
-                    put_line(to_string("device: " & device_scratch.name));
+                    --put_line("device: " & to_string(device_scratch.name)); -- dbg
                     append(list_of_devices,device_scratch); -- add device to list
 
                     -- purge device contents for next spin
                     device_scratch.name := to_bounded_string(""); 
                     device_scratch.packge := to_bounded_string("");
                     device_scratch.value := to_bounded_string("");
-                else
+				else
+					prog_position	:= 60;
                     case device_attribute_next is
                         when name => 
                             device_scratch.name := to_bounded_string(get_field(text_in => to_string(line), position => 1));
@@ -184,21 +438,24 @@ begin
             end if;
 
             -- READ NETS (NAME, PINS)
-            if not net_entered then
+			if not net_entered then
+				prog_position	:= 80;
                 if get_field(text_in => to_string(line), position => 1) = "(" then
                     net_entered := true;
                     net_item_next := name;
                 end if;
-            else -- we are inside a net section
+			else -- we are inside a net section
+				prog_position	:= 90;				
                 if get_field(text_in => to_string(line), position => 1) = ")" then
                     net_entered := false; -- we are leaving a net section
-                    put_line(to_string("net: " & net_scratch.name));
+                    --put_line("net: " & to_string(net_scratch.name)); -- dbg
                     type_list_of_nets.append(list_of_nets,net_scratch); -- add net to list
 
                     -- purge net contents for next spin
                     net_scratch.name := to_bounded_string(""); -- clear name
                     type_list_of_pins.delete(net_scratch.pins,1,type_list_of_pins.length(net_scratch.pins)); -- clear pin list
-                else
+				else
+					prog_position	:= 100;
                     case net_item_next is
                         when name => 
                             net_scratch.name := to_bounded_string(get_field(text_in => to_string(line), position => 1));                        
@@ -209,9 +466,20 @@ begin
             end if;
         end if;
     end loop;
+	set_input(standard_input);
+	close(file_cad_net_list);
 
-    test_assembly_variants;
-    
+    manage_assembly_variants;
+
+--	set_output(file_skeleton);
+	put_line(file_skeleton,section_mark.endsection);
+
+	-- write section netlist_skeleton
+	write_skeleton;
+
+	close(file_skeleton);
+	
+	
 	exception
 -- 		when constraint_error => 
 
@@ -233,5 +501,6 @@ begin
 					put_line(exception_name(event));
 					put(exception_message(event)); new_line;
 					put_line("program error at position " & natural'image(prog_position));
+					put_line("line in netlist" & natural'image(line_counter));
 			end case;
 end impprotel;
