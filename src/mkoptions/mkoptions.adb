5e36cdeb44e753fc1c1ce8fba33f34736a5289d5
Mario Blunk <mario.blunk@blunk-electronic.de>
2017-04-12 11:42:46 +0200

    reading bridges and connector pairs complete

--- src/mkoptions/mkoptions.adb
+++ src/mkoptions/mkoptions.adb
@@ -35,6 +35,7 @@ with ada.text_io;				use ada.text_io;
 with ada.characters.handling; 	use ada.characters.handling;
 with ada.containers;        	use ada.containers;
 with ada.containers.vectors;
+with ada.containers.indefinite_vectors;
 
 --with ada.strings.unbounded; use ada.strings.unbounded;
 with ada.strings.bounded; 		use ada.strings.bounded;
@@ -56,6 +57,9 @@ procedure mkoptions is
 	use type_name_database;
 	use type_name_file_options;
 	use type_name_file_routing;
+
+	use type_device_name;
+	use type_pin_name;	
 	
 	version			: String (1..3) := "030";
 	
@@ -1149,35 +1153,56 @@ procedure mkoptions is
 	procedure write_options_file_header is
 	begin
 		set_output(file_options);
-		put ("-- THIS IS AN OPTIONS FILE FOR DATA BASE '" & type_name_database.to_string(name_file_database) & "'"); new_line;
-		put ("-- created by mkoptions version " & version); new_line;	
-		put ("-- date       : " ); put (date_now); new_line; 
-		put ("-- Please modifiy net classes and primary/secondary dependencies according to your needs."); new_line (2);
+		put_line ("-- THIS IS AN OPTIONS FILE FOR " & to_upper(text_identifier_database) & row_separator_0 & to_string(name_file_database));
+		put_line ("-- created by " & name_module_mkoptions & " version " & version);	
+		put_line ("-- date " & date_now);
+		put_line ("-- Please modifiy net classes and primary/secondary dependencies according to your needs."); 
+		new_line;
 		set_output(standard_output);
 	end write_options_file_header;
 
 
-    length_of_device_name : constant positive := 100;
-    package type_device_name is new generic_bounded_length(length_of_device_name);
-	use type_device_name;
+--     length_of_device_name : constant positive := 100;
+--     package type_device_name is new generic_bounded_length(length_of_device_name);
+-- 	use type_device_name;
+-- 
+--     length_of_pin_name : constant positive := 10;
+--     package type_pin_name is new generic_bounded_length(length_of_pin_name);
+--     use type_pin_name;
 
-    length_of_pin_name : constant positive := 10;
-    package type_pin_name is new generic_bounded_length(length_of_pin_name);
-    use type_pin_name;
-
-	type type_bridge is record
+	length_pins_of_bridge_max : constant := 1 + 2 * pin_name_length; -- for something like "1-8" or "999-999"
+	package type_pins_of_bridge is new generic_bounded_length(length_pins_of_bridge_max);
+	
+	type type_pin is record
+		name			: type_pin_name.bounded_string;
+		processed		: boolean := false;
+		connected		: boolean := false;
+	end record;
+	
+	type type_bridge_preliminary is tagged record
 		name			: type_device_name.bounded_string;
 		wildcards		: boolean := false; -- true if name contains asterisks (*) or quesition marks (?)
-		pin_a			: type_pin_name.bounded_string;
-		pin_b			: type_pin_name.bounded_string;
-		pin_a_processed : boolean := false;
-		pin_b_processed : boolean := false;
-		pin_a_connected : boolean := false;
-		pin_b_connected : boolean := false;
-		--pin_ct			: natural := 0; -- CS: range 0..2 ?
-		is_array		: boolean := false; -- indicates whether bridge is part of an array
+		pin_a			: type_pin;
+		pin_b			: type_pin;
 	end record;
-	package type_list_of_bridges is new vectors ( index_type => positive, element_type => type_bridge);
+
+	type type_bridge_within_array is record -- like "1-8 2-7"
+		pin_a			: type_pin;
+		pin_b			: type_pin;
+	end record;
+	package type_list_of_bridges_within_array is new vectors 
+		(index_type => positive, element_type => type_bridge_within_array);
+	list_of_bridges_within_array_preliminary : type_list_of_bridges_within_array.vector;
+	use type_list_of_bridges_within_array;
+	
+	type type_bridge ( is_array : boolean) is new type_bridge_preliminary with record
+		case is_array is
+			when true => list_of_bridges : type_list_of_bridges_within_array.vector;
+			when false => null;
+		end case;
+	end record;
+	package type_list_of_bridges is new indefinite_vectors 
+		( index_type => positive, element_type => type_bridge);
 	use type_list_of_bridges;
 	list_of_bridges : type_list_of_bridges.vector;
 	
@@ -1197,22 +1222,84 @@ procedure mkoptions is
 	list_of_connector_pairs : type_list_of_connector_pairs.vector;
 
 	function has_wildcards (device : in type_device_name.bounded_string) return boolean is
+		asterisks_count 		: natural := type_device_name.count(device,"*");
+		question_marks_count	: natural := type_device_name.count(device,"?");
+		wildcards_found			: boolean := false;
 	begin
+		if asterisks_count > 0 then
+			wildcards_found := true;
+		end if;
 
-		return false;
+		if question_marks_count > 0 then
+			wildcards_found := true;
+		end if;
+		
+		return wildcards_found;
 	end has_wildcards;
-	
-	procedure read_mkoptions is
-		type_line_length_max	: constant natural := 1000;
-		package type_line_of_file is new generic_bounded_length(type_line_length_max); use type_line_of_file;
-		line					: type_line_of_file.bounded_string;
-		field_count				: natural;
-		section_connectors_entered : boolean := false;
-		section_bridges_entered : boolean := false;
 
-		conpair_preliminary : type_connector_pair;
-		bridge_preliminary	: type_bridge;
-	begin
+	function read_bridges_of_array (
+		line 		: in string; -- contains something like "RN303 array 1-2 3-4 5-6 7-8"
+		field_count : in positive;
+		line_counter: in positive) 
+		return type_list_of_bridges_within_array.vector is
+		list_of_bridges : type_list_of_bridges_within_array.vector;
+
+		use type_pins_of_bridge; 
+		field : type_pins_of_bridge.bounded_string; -- something like "1-8"
+
+		bridge 		: type_bridge_within_array;
+		separator	: constant string (1..1) := "-";
+ 		pos_sep 	: positive; -- position of separator "-" 
+	begin -- read_bridges_of_array
+		for i in 3..field_count loop -- we start reading in field 3 
+			field := to_bounded_string(get_field_from_line(line,i));
+			if type_pins_of_bridge.count(field,separator) = 1 then -- there must be a single separator
+				pos_sep 			:= type_pins_of_bridge.index(field,separator); -- get position of separator
+
+				-- get pin names left and right of separator
+				bridge.pin_a.name	:= to_bounded_string(type_pins_of_bridge.slice(field, 1, pos_sep-1));
+				bridge.pin_b.name	:= to_bounded_string(type_pins_of_bridge.slice(field, pos_sep+1, length(field)));
+
+				write_message (
+					file_handle => file_mkoptions_messages,
+					identation => 3,
+					text => "pins: " & to_string(bridge.pin_a.name) & separator & to_string(bridge.pin_b.name),
+					console => false);
+
+				-- CS: check multiple occurences of pin names and bridges
+				
+				append(list_of_bridges, bridge);
+				
+			else
+				-- put a final linebreak at end of line in logfile
+				new_line (file_mkoptions_messages);
+				
+				write_message (
+					file_handle => file_mkoptions_messages,
+					text => message_error & "in line" & positive'image(line_counter) & ": " 
+						& "A single separator '" & separator & "' expected between pin names ! "
+						& "Example: RN4 array 1-8 2-7 3-6 4-5",
+					console => true);
+				raise constraint_error;
+			end if;
+			
+		end loop;
+		return list_of_bridges;
+	end read_bridges_of_array;
+	
+	procedure read_mkoptions_configuration is
+		type_line_length_max		: constant natural := 1000;
+		package type_line_of_file is new generic_bounded_length(type_line_length_max); use type_line_of_file;
+		line						: type_line_of_file.bounded_string;
+		line_counter				: natural := 0;
+		field_count					: natural;
+		section_connectors_entered	: boolean := false;
+		section_bridges_entered 	: boolean := false;
+		
+		conpair_preliminary 		: type_connector_pair;
+		bridge_preliminary			: type_bridge_preliminary;
+		bridge_is_array				: boolean := false;
+	begin -- read_mkoptions_configuration
 		write_message (
 			file_handle => file_mkoptions_messages,
 			text => "reading file " & name_file_mkoptions_conf & "...",
@@ -1221,8 +1308,9 @@ procedure mkoptions is
 		open (file => file_mkoptions, mode => in_file, name => name_file_mkoptions_conf);
 		set_input(file_mkoptions);
 		while not end_of_file loop
-			line 		:= to_bounded_string(remove_comment_from_line(get_line));
-			field_count	:= get_field_count(to_string(line));
+			line 			:= to_bounded_string(remove_comment_from_line(get_line));
+			line_counter	:= line_counter + 1;
+			field_count		:= get_field_count(to_string(line));
 			if field_count > 0 then -- skip empty lines
 				--				put_line(extended_string.to_string(line));
 
@@ -1248,33 +1336,48 @@ procedure mkoptions is
 
 						append(list_of_connector_pairs,conpair_preliminary);
 					else
-						-- read names of connectors
- 						conpair_preliminary.name_a	:= to_bounded_string(get_field_from_line(to_string(line),1));
- 						conpair_preliminary.name_b	:= to_bounded_string(get_field_from_line(to_string(line),2));
+						-- Read names of connectors.
+						-- There must be at least 2 fields per line (for connector A and B and mapping)
+						if field_count > 1 then 
+							conpair_preliminary.name_a	:= to_bounded_string(get_field_from_line(to_string(line),1));
+							conpair_preliminary.name_b	:= to_bounded_string(get_field_from_line(to_string(line),2));
 
-						-- report names in logfile
-						write_message (
-							file_handle => file_mkoptions_messages,
-							identation => 2, 
-							text => "A " & to_string(conpair_preliminary.name_a) 
-								 & " B " & to_string(conpair_preliminary.name_b),
-							lf => false,
-							console => false);
+							-- report names in logfile
+							write_message (
+								file_handle => file_mkoptions_messages,
+								identation => 2, 
+								text => "A " & to_string(conpair_preliminary.name_a) 
+									& " B " & to_string(conpair_preliminary.name_b),
+								lf => false,
+								console => false);
 
-						-- If mapping provided in a 3rd field, read it and report in logfile. 
-						-- If not provided, assume default mapping.
-						if field_count > 2 then
-							conpair_preliminary.mapping	:= type_connector_mapping'value(get_field_from_line(to_string(line),3));
-							-- CS: helpful message when invalid mapping
+							-- If mapping provided in a 3rd field, read it and report in logfile. 
+							-- If not provided, assume default mapping.
+							if field_count > 2 then
+								conpair_preliminary.mapping	:= type_connector_mapping'value(get_field_from_line(to_string(line),3));
+								-- CS: helpful message when invalid mapping
+							else
+								conpair_preliminary.mapping := connector_mapping_default;
+							end if;
+
+							write_message (
+								file_handle => file_mkoptions_messages,
+								identation => 1, 
+								text => "mapping " & type_connector_mapping'image(conpair_preliminary.mapping),
+								console => false);
 						else
-							conpair_preliminary.mapping := connector_mapping_default;
-						end if;
+							-- put a final linebreak at end of line in logfile
+							new_line (file_mkoptions_messages);
+							
+							write_message (
+								file_handle => file_mkoptions_messages,
+								text => message_error & "in line" & positive'image(line_counter) & ": " 
+									& "Connector pair expected ! "
+									& "Example: main_X1 sub_X1",
+								console => true);
+							raise constraint_error;
 
-						write_message (
-							file_handle => file_mkoptions_messages,
-							identation => 1, 
-							text => "mapping " & type_connector_mapping'image(conpair_preliminary.mapping),
-							console => false);
+						end if;
 					end if;
 				end if;
 
@@ -1298,7 +1401,15 @@ procedure mkoptions is
 					if get_field_from_line(to_string(line),1) = section_mark.endsection then -- we are leaving section bridges
 						section_bridges_entered := false; 
 
---						append(list_of_bridges,bridge_scratch);
+						case bridge_is_array is
+							when false =>
+								append(list_of_bridges, (bridge_preliminary with is_array => false));
+							when true =>
+								append(list_of_bridges, (bridge_preliminary with 
+															is_array => true, 
+															list_of_bridges => list_of_bridges_within_array_preliminary));
+						end case;
+						
 					else
 						-- read bridges like:
 						--  R4
@@ -1307,7 +1418,6 @@ procedure mkoptions is
 
 						-- read name of bridge and check if name contains wildcards
 						bridge_preliminary.name := to_bounded_string(get_field_from_line(to_string(line),1));
-						bridge_preliminary.wildcards := has_wildcards(bridge_preliminary.name);
 
 						-- report bridge in logfile
 						write_message (
@@ -1317,18 +1427,66 @@ procedure mkoptions is
 							lf => false,
 							console => false);
 
-						-- field #2 may indicated that this is an array
+						bridge_preliminary.wildcards := has_wildcards(bridge_preliminary.name);						
+						if bridge_preliminary.wildcards then
+							write_message (
+								file_handle => file_mkoptions_messages,
+								identation => 1,
+								text => "has wildcards",
+								lf => false,
+								console => false);
+						end if;
+						
+						-- Field #2 may indicate that this is an array.
 						if field_count > 1 then
 							if get_field_from_line(to_string(line),2) = options_keyword_array then
-								bridge_preliminary.is_array := true;
+								bridge_is_array := true;
 
 								write_message (
 									file_handle => file_mkoptions_messages,
 									identation => 1,
 									text => "is array",
-									lf => false,
 									console => false);
+
+								-- build a preliminary list of bridges from fields after "array"
+								if field_count > 2 then
+									list_of_bridges_within_array_preliminary := read_bridges_of_array(
+										line 			=> to_string(line),
+										field_count 	=> field_count,
+										line_counter	=> line_counter);
+								else
+									-- put a final linebreak at end of line in logfile
+-- 									new_line (file_mkoptions_messages);
+									
+									write_message (
+										file_handle => file_mkoptions_messages,
+										text => message_error & "in line" & positive'image(line_counter) & ": " 
+											& "Bridges within array expected ! Example: RN4 array 1-8 2-7 3-6 4-5",
+										console => true);
+									raise constraint_error;
+								end if;
+								
+							else
+								-- put a final linebreak at end of line in logfile
+								new_line (file_mkoptions_messages);
+								
+								write_message (
+									file_handle => file_mkoptions_messages,
+									text => message_error & "in line" & positive'image(line_counter) & ": " 
+										& "Keyword '" & options_keyword_array & "' expected after device name ! "
+										& "Example: RN4 array 1-8 2-7 3-6 4-5",
+									console => true);
+								raise constraint_error;
 							end if;
+						else 
+							-- It is not an array of bridges.
+							bridge_is_array := false;
+
+-- 							-- Purge preliminary list of bridges within array for next spin.							
+-- 							if length(list_of_bridges_within_array_preliminary) > 0 then
+-- 								delete(list_of_bridges_within_array_preliminary,1,length(list_of_bridges_within_array_preliminary));
+-- 							end if;
+
 						end if;
 
 						-- put a final linebreak at end of line in logfile
@@ -1340,9 +1498,31 @@ procedure mkoptions is
 			end if;
 		end loop;	
 		close(file_mkoptions);
-	end read_mkoptions;
+	end read_mkoptions_configuration;
 		
-
+	procedure write_statistics is
+	begin
+-- 		conpair_ct := count_connector_pairs;
+-- 		put ("-- connector pairs  :" & Natural'Image(conpair_ct)); new_line;	
+-- 
+-- 		bridge_ct := count_bridges;
+-- 		--put ("-- bridges         :" & Natural'Image(bridge_ct)); new_line;	
+-- 
+-- 		net_ct := count_nets;
+-- 		put ("-- net count total  :" & Natural'Image(net_ct) & " (incl. non-bs nets)"); new_line;
+-- 		put ("--                    NOTE: Non-bs nets are commented and shown as supplementary information only."); new_line; 
+-- 		put ("--                          Don't waste your time editing their net classes !"); new_line;
+-- 
+-- 		new_line(standard_output);		
+-- 		if make_netlist
+-- 			(
+-- 			con_pair_list_in => make_conpair_list,
+-- 			bridge_list_in => make_bridge_list
+-- 			)
+-- 		then null; end if;
+		null;
+	end write_statistics;
+	
 -------- MAIN PROGRAM ------------------------------------------------------------------------------------
 
 begin
@@ -1436,32 +1616,14 @@ begin
 			text => message_error & "No configuration file '" & name_file_mkoptions_conf & "' found !",
 			console => true);
 		raise constraint_error;
-	else
-        read_mkoptions;
--- 
--- 
--- 		conpair_ct := count_connector_pairs;
--- 		put ("-- connector pairs  :" & Natural'Image(conpair_ct)); new_line;	
--- 
--- 		bridge_ct := count_bridges;
--- 		--put ("-- bridges         :" & Natural'Image(bridge_ct)); new_line;	
--- 
--- 		net_ct := count_nets;
--- 		put ("-- net count total  :" & Natural'Image(net_ct) & " (incl. non-bs nets)"); new_line;
--- 		put ("--                    NOTE: Non-bs nets are commented and shown as supplementary information only."); new_line; 
--- 		put ("--                          Don't waste your time editing their net classes !"); new_line;
--- 
--- 		new_line(standard_output);		
--- 		if make_netlist
--- 			(
--- 			con_pair_list_in => make_conpair_list,
--- 			bridge_list_in => make_bridge_list
--- 			)
--- 		then null; end if;
-	
-
 	end if;
+	
+	read_mkoptions_configuration;
+-- CS:	write_statistics;
 
+
+
+	
 
 	close(file_options);
 
