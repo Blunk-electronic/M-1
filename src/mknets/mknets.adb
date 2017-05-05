bf14c684248071bd42f7ee661a6631318864f5c4
Mario Blunk <mario.blunk@blunk-electronic.de>
2017-05-05 09:27:09 +0200

    variable skeleton added

--- src/mknets/mknets.adb
+++ src/mknets/mknets.adb
@@ -73,184 +73,6 @@ procedure mknets is
 	use type_pin_name;
 	use type_port_name;
 
-	use pin_container;
-	use net_container;
-	
--- 	type type_skeleton_pin is record
--- 			device_name			: type_device_name.bounded_string;
--- 			device_class		: type_device_class := device_class_default;
--- 			device_value		: type_device_value.bounded_string;
--- 			device_package		: type_package_name.bounded_string;
--- 			device_pin_name		: type_pin_name.bounded_string;
--- 		end record;
--- 	package pin_container is new doubly_linked_lists(element_type => type_skeleton_pin);
--- 	use pin_container;
--- 	
--- 	type type_skeleton_net is record
--- 			name			: type_net_name.bounded_string;
--- 			--class			: type_net_class;
--- 			--pin_count		: positive;
--- 			pin_list		: pin_container.list;
--- 			--pin_cursor		: pin_container.cursor;
--- 		end record;
--- 	package net_container is new doubly_linked_lists(element_type => type_skeleton_net);
--- 	use net_container;
--- 	netlist : net_container.list;
-
--- 	procedure read_skeleton is
--- 	-- Reads the skeleton and adds nets in container netlist.
--- 		line_of_file 			: type_universal_string.bounded_string;
--- 		line_counter			: natural := 0;
--- 		section_netlist_entered	: boolean := false;
--- 		subsection_net_entered	: boolean := false;
--- 		pin_scratch				: type_skeleton_pin;
--- 		pinlist					: pin_container.list;
--- 		net_scratch				: type_skeleton_net;
--- 
--- 		procedure put_faulty_line is
--- 		begin
--- 			put_line(message_error & "in skeleton line" & natural'image(line_counter));
--- 		end put_faulty_line;
--- 		
--- 	begin -- read_skeleton
--- 		write_message (
--- 			file_handle => file_mknets_messages,
--- 			identation => 1,
--- 			text => "reading skeleton ...", 
--- 			lf => false,
--- 			console => true);
--- 		new_line(file_mknets_messages);
--- 		
--- 		if not exists(name_file_skeleton_default) then
--- 			write_message (
--- 				file_handle => file_mknets_messages,
--- 				text => message_error & name_file_skeleton & " not found !", 
--- 				console => true);
--- 			raise constraint_error;
--- 		end if;
--- 		
--- 		open(file => file_skeleton, name => name_file_skeleton_default, mode => in_file);
--- 		set_input(file_skeleton);
--- 		while not end_of_file
--- 		loop
--- 			prog_position := 1000;
--- 			line_counter := line_counter + 1;
--- 
--- 			-- progress bar
--- 			if (line_counter rem 400) = 0 then -- put a dot every 400 lines of skeleton
--- 				put(standard_output,".");
--- 			end if;
--- 			
--- 			line_of_file := to_bounded_string(remove_comment_from_line(get_line));
--- 
--- 			if get_field_count(to_string(line_of_file)) > 0 then -- if line contains anything
--- 				if not section_netlist_entered then
--- 					if get_field_from_line(to_string(line_of_file),1) = section_mark.section then
--- 						if get_field_from_line(to_string(line_of_file),2) = text_skeleton_section_netlist then
--- 							section_netlist_entered := true;
--- 						end if;
--- 					end if;
--- 				else
--- 					if get_field_from_line(to_string(line_of_file),1) = section_mark.endsection then
--- 						section_netlist_entered := false;
--- 					else
--- 						-- process netlist content
--- 
--- 						-- wait for net header
--- 						if not subsection_net_entered then
--- 							-- The net header starts with "SubSection". The 3rd field must read "class".
--- 							if get_field_from_line(to_string(line_of_file),1) = section_mark.subsection then
--- 								
--- 								-- save net name
--- 								net_scratch.name := to_bounded_string(get_field_from_line(to_string(line_of_file),2));
--- 
--- 								write_message (
--- 									file_handle => file_mknets_messages,
--- 									identation => 2,
--- 									text => "net " & to_string(net_scratch.name), 
--- 									console => false);
--- 								
--- 								-- check for keyword "class"
--- 								if get_field_from_line(to_string(line_of_file),3) = text_udb_class then
--- 									null;
--- 									--put_line(extended_string.to_string(line_of_file));
--- 								else
--- 									put_line(message_error & "missing keyword " & enclose_in_quotes(text_udb_class));
--- 									put_faulty_line;
--- 									raise constraint_error;
--- 								end if;
--- 
--- 								-- check for default class 
--- 								if get_field_from_line(to_string(line_of_file),4) = type_net_class'image(net_class_default) then
--- 									--net_scratch.class := NA;
--- 									null;
--- 								else
--- 									put_line(message_error & "expecting default net class " & type_net_class'image(net_class_default));
--- 									put_faulty_line;
--- 									raise constraint_error;
--- 								end if;
--- 
--- 								subsection_net_entered := true;
--- 							end if;
--- 							
--- 						else -- Read pins untile net footer reached. The net footer is "EndSubSection".
--- 							-- When net footer reached:
--- 							-- 1. save pinlist in net_scratch.pin_list
--- 							-- 2. check for one-pin nets
--- 							-- 3. append net_scratch to container netlist
--- 							if get_field_from_line(to_string(line_of_file),1) = section_mark.endsubsection then --net footer reached
--- 								subsection_net_entered := false;
--- 								net_scratch.pin_list := pinlist;
--- 
--- 								-- check for one-pin net
--- 								if length(pinlist) = 1 then
--- 									write_message (
--- 										file_handle => file_mknets_messages,
--- 										text => message_warning & "net " & to_string(net_scratch.name) & " has only one pin !", 
--- 										console => false);
--- 								end if;
--- 								
--- 								append(container => netlist, new_item => net_scratch);
--- 								clear(pinlist); -- clear pinlist for next net
--- 							else
--- 								-- net footer not reached yet -> check field count and read pins
--- 								if get_field_count(to_string(line_of_file)) = skeleton_field_count_pin then
--- 									-- process pins of net and add to container pin_list
--- 									pin_scratch.device_name := to_bounded_string(get_field_from_line(to_string(line_of_file),1));
--- 									--pin_scratch.device_class := type_device_class'value(get_field_from_line(line_of_file,2)); -- CS
--- 									pin_scratch.device_value := to_bounded_string(get_field_from_line(to_string(line_of_file),3));
--- 									pin_scratch.device_package := to_bounded_string(get_field_from_line(to_string(line_of_file),4));
--- 									pin_scratch.device_pin_name := to_bounded_string(get_field_from_line(to_string(line_of_file),5));
--- 
--- 									write_message (
--- 										file_handle => file_mknets_messages,
--- 										identation => 3,
--- 										text => "device/pin " & to_string(pin_scratch.device_name) & row_separator_0
--- 											& to_string(pin_scratch.device_pin_name),
--- 										console => false);
--- 									
--- 									append(container => pinlist, new_item => pin_scratch);
--- 								else
--- 									put_line(message_error & "invalid number of fields found !");
--- 									put_faulty_line;
--- 								end if;
--- 							end if;
--- 
--- 						end if;
--- 					end if;
--- 				end if;
--- 
--- 			end if;
--- 		end loop;
--- 
--- 		write_message (
--- 			file_handle => file_mknets_messages,
--- 			text => "", 
--- 			console => true);
--- 		
--- 	end read_skeleton;
-	
-
 	function get_cell_info (
 	-- Returns the cell info like "pb01_11 | 20 bc_1 input x | 19 bc_1 output3 x 18 0 z" for a given bic and pin.
 	-- Even if there are no cells (e.g. linkage pins) the port will be returned.
@@ -399,14 +221,18 @@ procedure mknets is
 		return to_string(scratch);
 	end get_cell_info;
 	
-	procedure write_netlist is
-	-- Unpacks the items of container netlist.
-		net_cursor 		: net_container.cursor;		
+	procedure write_netlist (skeleton : in type_skeleton) is
+	-- Writes the netlist of given skeleton in (currently open) skeleton file.
+
+		use type_skeleton_netlist;
+		
+		net_cursor 		: type_skeleton_netlist.cursor;		
 		net_scratch		: type_skeleton_net;
 		net_counter		: natural := 0;
 
 		procedure write_net is
-			pin_cursor		: pin_container.cursor;
+			use type_skeleton_pinlist;
+			pin_cursor		: type_skeleton_pinlist.cursor;
 			pin_scratch		: type_skeleton_pin;
 			bic 			: type_bscan_ic;
 			
@@ -438,8 +264,6 @@ procedure mknets is
 			
 		begin -- write_net
 			-- write net header like "SubSection ex_GPIO_2 class NA"
-            --put_line(standard_output, universal_string_type.to_string( net_scratch.name));
-			
 			put_line(row_separator_0 & section_mark.subsection & row_separator_0 &
 					 to_string(net_scratch.name) & row_separator_0 &
 					 text_udb_class & row_separator_0 & type_net_class'image(net_class_default)
@@ -473,11 +297,11 @@ procedure mknets is
 			console => true);
 		new_line(file_mknets_messages);
 		
-		net_cursor 	:= first(netlist);
+		net_cursor 	:= first(skeleton.netlist);
 		net_scratch := element(net_cursor);
 
 		write_net;
-		while net_cursor /= last(netlist) loop
+		while net_cursor /= last(skeleton.netlist) loop
 			net_cursor 	:= next(net_cursor);
 			net_scratch := element(net_cursor);
 
@@ -530,18 +354,19 @@ procedure mknets is
 		close(file_database);
 	end copy_scanpath_configuration_and_registers;
 
-	procedure write_section_netlist_header is
+	procedure write_section_netlist_header (skeleton : in type_skeleton) is
+		use type_skeleton_netlist;
 	begin
 		new_line;
 		put_line(section_mark.section & row_separator_0 & section_netlist);
 		put_line(column_separator_0);
 		put_line("-- created by " & name_module_mknets & " version " & version);
 		put_line("-- date " & date_now); 
-		put_line("-- number of nets" & count_type'image(length(netlist)));
+		put_line("-- number of nets" & count_type'image(length(skeleton.netlist)));
 		new_line;
 	end write_section_netlist_header;
 
-	
+	skeleton : type_skeleton;
 
 -------- MAIN PROGRAM ------------------------------------------------------------------------------------
 
@@ -569,7 +394,6 @@ begin
 		 & to_string(name_file_database));
 	
 	prog_position	:= 50;
-	-- CS: set integrity check level
 	read_uut_database;
 	
 	-- create premilinary data base (contining scanpath_configuration and registers)
@@ -581,21 +405,19 @@ begin
 		);
 
 	prog_position	:= 80;	
-	--open(file_database_preliminary,out_file,name_file_database_preliminary);
 	set_output(file_database_preliminary);
 	
 	prog_position	:= 90;		
 	copy_scanpath_configuration_and_registers;
 
 	prog_position	:= 100;
-	read_skeleton;
+	skeleton := read_skeleton;
 
-	-- read skeleton
  	prog_position 	:= 110;
-	write_section_netlist_header;
+	write_section_netlist_header(skeleton);
 	
  	prog_position 	:= 120;	
-	write_netlist;
+	write_netlist(skeleton);
 
 	-- write section netlist footer
 	put_line (section_mark.endsection); new_line;
